180e086bd3d128a42031ebf78459c9bccd2efe0d
Novik <novik65@gmail.com>
2022-12-15 13:06:56 +0300

    fix menu

--- jet/utils.py
+++ jet/utils.py
@@ -443,6 +443,15 @@ def get_menu_items(context):
             return item
         app_list = list(map(map_item, original_app_list.values()))
 
+    for app in app_list:
+        has_perms = app.get('has_perms', True)
+        if has_perms:
+            for model in app['items']:
+                has_perms = has_perms and model.get('has_perms', True)
+                if not has_perms:
+                    break
+            app['has_perms'] = has_perms
+
     current_found = False
 
     for app in app_list:
